d6e0683ed4dc48b77c5d6495e37a3f2dd387555a
abdulaziz052 <killerabdulaziz60@gmail.com>
2024-07-12 10:51:32 +0500

    swiper with pagination end

--- public/js/app.js
+++ public/js/app.js
@@ -3,16 +3,27 @@ let resp = document.querySelector('#responsive-menu')
 let ham__btn = document.querySelector('#hamburger-menu')
 let close__btn = document.querySelector('#close-menu')
 let body = document.querySelector('body')
+let box = document.querySelectorAll('#box')
+let pagination = document.querySelector('.swiper-pagination1')
 
-ham__btn.addEventListener('click' , function() {
+pagination.innerHTML = 'of'
+
+ham__btn.addEventListener('click', function () {
     resp.classList.remove('-left-full')
     resp.classList.add('left-0')
     body.classList.add('overflow-hidden')
 })
-close__btn.addEventListener('click' , function() {
+close__btn.addEventListener('click', function () {
     resp.classList.remove('left-0')
     resp.classList.add('-left-full')
     body.classList.remove('overflow-hidden')
-
-    
 })
+box.forEach(function (item, index) {
+    item.addEventListener('click', function () {
+        box.forEach(function (item, index) {
+            item.classList.remove('active')
+        })
+        item.classList.add('active')
+    })
+})
+
