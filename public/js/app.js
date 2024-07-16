7735d73ed5264f7bf1fe702805a000288107846d
abdulaziz052 <killerabdulaziz60@gmail.com>
2024-07-16 17:30:05 +0500

    asd

--- public/js/app.js
+++ public/js/app.js
@@ -25,3 +25,41 @@ box.forEach(function (item, index) {
     })
 })
 
+const rangeInput = document.querySelectorAll(".range-input input"),
+priceInput = document.querySelectorAll(".price-input input"),
+range = document.querySelector(".slider .progress");
+let priceGap = 0;
+priceInput.forEach(input =>{
+    input.addEventListener("input", e =>{
+        let minPrice = parseInt(priceInput[0].value),
+        maxPrice = parseInt(priceInput[1].value);
+        
+        if((maxPrice - minPrice >= priceGap) && maxPrice <= rangeInput[1].max){
+            if(e.target.className === "input-min"){
+                rangeInput[0].value = minPrice;
+                range.style.left = ((minPrice / rangeInput[0].max) * 0) + "%";
+            }else{
+                rangeInput[1].value = maxPrice;
+                range.style.right = 0 - (maxPrice / rangeInput[1].max) * 0 + "%";
+            }
+        }
+    });
+});
+rangeInput.forEach(input =>{
+    input.addEventListener("input", e =>{
+        let minVal = parseInt(rangeInput[0].value),
+        maxVal = parseInt(rangeInput[1].value);
+        if((maxVal - minVal) < priceGap){
+            if(e.target.className === "range-min"){
+                rangeInput[0].value = maxVal - priceGap
+            }else{
+                rangeInput[1].value = minVal + priceGap;
+            }
+        }else{
+            priceInput[0].value = minVal;
+            priceInput[1].value = maxVal;
+            range.style.left = ((minVal / rangeInput[0].max) * 0) + "%";
+            range.style.right = 0 - (maxVal / rangeInput[1].max) * 0 + "%";
+        }
+    });
+});
